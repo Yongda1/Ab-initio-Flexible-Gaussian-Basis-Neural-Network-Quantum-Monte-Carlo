76fec48e328efc27b56b78a90a3287ac52c2c9aa
Yongda Huang <yongdahuang123@gmail.com>
2024-07-23 08:44:52 +0200

    We start to write the new envelop functions.19/07/2024. We already check the shape of the input array and output array. It is working well. we are still thinking about how to construct the flexiable gaussian basis.

--- AIQMC/envelopes.py
+++ AIQMC/envelopes.py
@@ -46,7 +46,7 @@ class Envelope:
 
 '''
 pos = jnp.array([1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 0.5])
-atoms = jnp.array([[0, 0, 0], [1, 1, 1]])
+atoms = jnp.array([[0, 0, 0], [0.2, 0.2, 0.2]])
 ae, ee = construct_input_features(pos, atoms, ndim=3)
 print("ae", ae)
 print("ee", ee)
@@ -77,7 +77,7 @@ def make_GTO_envelope():
         #print("xi", xi)
         return {"xi": xi}
 
-    def apply(ae: jnp.ndarray, atoms: jnp.ndarray, xi: jnp.ndarray) -> jnp.ndarray:
+    def apply(ae: jnp.ndarray, atoms: jnp.ndarray, xi: jnp.ndarray, natoms: int, nelectrons: int) -> jnp.ndarray:
         """the input for the apply function must be the r_effective. It should be scalar.
         we need assign the electrons to atoms. Now, we have 4 electrons, each atom has two electrons.
         The number one and number three electrons belong to first_atom. The spin configuration is up up down down.
@@ -85,9 +85,39 @@ def make_GTO_envelope():
         electron_coordinates = ae[0][0] + atoms[0]
         r_ae = jnp.linalg.norm(ae, axis=-1)
         print("r_ae", r_ae)
+        print("ae", ae)
         print("xi", xi)
+        """we need convert the coordinates to spherical coordinates."""
+        def to_spherical(x: jnp.ndarray):
+            """Converts a cartesian coordinate (x, y, z) into a spherical one (radius, theta, phi)."""
+            radius = jnp.linalg.norm(x)
+            phi_ = jnp.arccos(x[2]/radius)
+            theta_ = jnp.arctan(x[1]/x[0])
+            return jnp.array([radius, theta_, phi_])
 
-        return
+        ae = jnp.reshape(ae, (natoms * nelectrons, 3))
+        print("ae", ae)
+        """here, we used vmap to vectorize the function."""
+        to_spherical1 = jax.vmap(to_spherical, axis_size=1, out_axes=0)
+        spherical_coordinates = to_spherical1(ae)
+        print("spherical", spherical_coordinates)
+        theta_coordinates = spherical_coordinates[:, 1]
+        phi_coordinates = spherical_coordinates[:, 2]
+        print("theta_coordinates", theta_coordinates)
+        print("phi_coordinates", phi_coordinates)
+        """here, n is L, i.e. the angular momentum quantum number."""
+        print(spherical_coordinates[0][1], spherical_coordinates[0][2])
+        angular_value_00 = sph_harm(n=jnp.array([0]), m=jnp.array([0]), theta=theta_coordinates, phi=phi_coordinates)
+        angular_value_1_1 = sph_harm(n=jnp.array([1]), m=jnp.array([-1]), theta=theta_coordinates, phi=phi_coordinates)
+        angular_value_10 = sph_harm(n=jnp.array([1]), m=jnp.array([0]), theta=theta_coordinates, phi=phi_coordinates)
+        angular_value_11 = sph_harm(n=jnp.array([1]), m=jnp.array([1]), theta=theta_coordinates, phi=phi_coordinates)
+        angular_value = jnp.array([angular_value_00, angular_value_1_1, angular_value_10, angular_value_11])
+        #print("angular_value", angular_value)
+        angular_value = jnp.transpose(angular_value)
+        print("-----------------------------")
+        print("angular_value", angular_value)
+
+        return angular_value
 
     return init, apply
 
@@ -95,4 +125,4 @@ def make_GTO_envelope():
 init, apply = make_GTO_envelope()
 xi = init(natom=2, nelectrons=4, ndim=3, order=3)
 print("xi", xi)
-output = apply(ae, atoms, xi)
+output = apply(ae, atoms, xi, natoms=2, nelectrons=4, )
