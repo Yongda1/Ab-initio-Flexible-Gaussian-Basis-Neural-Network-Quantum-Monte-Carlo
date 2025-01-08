debug H2 calculation to check the workflow is correct or not.

AIQMCrelease1/MonteCarloSample/mcstep.py
@@ -97,11 +97,17 @@ def generate_batch_key(batch_size: int):
     return get_keys
 
 
-def main_monte_carlo(f: nn.AINetLike, key: chex.PRNGKey, params: nn.ParamTree, batch_size: int,):
+def main_monte_carlo(f: nn.AINetLike,
+                     key: chex.PRNGKey,
+                     params: nn.ParamTree,
+                     batch_size: int,
+                     tstep: float,
+                     ndim: int,
+                     nelectrons: int):
     """create mont carlo sample loop. One loop is used here. However, we should circumvent it. Later, we optimize it."""
     generate_keys = generate_batch_key(batch_size=batch_size)
     generate_keys_parallel = jax.pmap(generate_keys)
-    mc = monte_carlo(logwavefunction=f, tstep=0.1, ndim=3, nelectrons=16)
+    mc = monte_carlo(logwavefunction=f, tstep=tstep, ndim=ndim, nelectrons=nelectrons)
     mc_parallel = jax.pmap(jax.vmap(mc, in_axes=(None, 0, 0)))
 
     def mc_step(nsteps: int, data: nn.AINetData):

AIQMCrelease1/main/main_all_electrons.py
@@ -1,6 +1,3 @@
-"""here, we learn how to use kfac and test it."""
-
-
 import time
 from typing import Optional, Tuple, Union
 from absl import logging
@@ -16,12 +13,12 @@ from AIQMCrelease1.wavefunction import nn
 from AIQMCrelease1.MonteCarloSample import mcstep
 from AIQMCrelease1.Loss import pploss as qmc_loss_functions
 from AIQMCrelease1 import constants
-from AIQMCrelease1.Energy import pphamiltonian
+from AIQMCrelease1.Energy import hamiltonian
 from AIQMCrelease1 import curvature_tags_and_blocks
 import functools
 
 
-def _assign_spin_configuration(nalpha: int, nbeta: int, batch_size: int=1) -> jnp.ndarray:
+def _assign_spin_configuration(nalpha: int, nbeta: int, batch_size: int = 1) -> jnp.ndarray:
     spins = jnp.concatenate((jnp.ones(nalpha), -jnp.ones(nbeta)))
     return jnp.tile(spins[None], reps=(batch_size, 1))
 
@@ -47,21 +44,24 @@ def init_electrons(key, structure: jnp.array, atoms: jnp.array, charges: jnp.arr
     electrons_positions_batch += (jax.random.normal(subkey, shape=electrons_positions_batch.shape) * init_width)
     "we need think about this. We need assign the spin configurations to electrons.12.08.2024."
     spins_no_batch = electrons
-    #spins = jnp.repeat(jnp.reshape(spins, (1, -1)), batch_size, )
+    # spins = jnp.repeat(jnp.reshape(spins, (1, -1)), batch_size, )
     return electrons_positions_batch, spins_no_batch
 
 
 OptimizerState = Union[optax.OptState, kfac_jax.Optimizer.State]
 
-OptUpdateResults = Tuple[nn.ParamTree, Optional[OptimizerState], jnp.array, Optional[qmc_loss_functions.AuxiliaryLossData]]
+OptUpdateResults = Tuple[
+    nn.ParamTree, Optional[OptimizerState], jnp.array, Optional[qmc_loss_functions.AuxiliaryLossData]]
 
 
 class OptUpdate(Protocol):
-    def __call__(self, params: nn.ParamTree, data: nn.AINetData, opt_state: optax.OptState, key: chex.PRNGKey) -> OptUpdateResults:
+    def __call__(self, params: nn.ParamTree, data: nn.AINetData, opt_state: optax.OptState,
+                 key: chex.PRNGKey) -> OptUpdateResults:
         """Evaluates the loss and gradients and updates the parameters accordingly."""
 
 
-StepResults = Tuple[nn.AINetData, nn.ParamTree, Optional[optax.OptState], jnp.ndarray, qmc_loss_functions.AuxiliaryLossData,]
+StepResults = Tuple[
+    nn.AINetData, nn.ParamTree, Optional[optax.OptState], jnp.ndarray, qmc_loss_functions.AuxiliaryLossData,]
 
 
 class Step(Protocol):
@@ -69,61 +69,57 @@ class Step(Protocol):
             -> StepResults:
         """Performs one set of MCMC moves and an optimization step."""
 
-    
     """Returns an OptUpdate function for performing a parameter update.
     So far ,we have not solved the spin configuration problem yet. But we got one more task about writing the loss function.
     Let's go back to main.py 14.08.2024. We cannot finished all functions now. Because we need guarrante all input data format fixed and
     Loss.py, hamiltonian.py, utils.py and pseudopotential.py form an entire part. So, next fews steps, we need move stepy by step."""
 
 
-def make_opt_update_step(evaluate_loss: qmc_loss_functions.LossAINet, optimizer: optax.GradientTransformation) -> OptUpdate:
+def make_opt_update_step(evaluate_loss: qmc_loss_functions.LossAINet,
+                         optimizer: optax.GradientTransformation) -> OptUpdate:
     loss_and_grad = jax.value_and_grad(evaluate_loss, argnums=0, has_aux=True)
 
-    def opt_update(params: nn.ParamTree, data: nn.AINetData, opt_state: Optional[optax.OptState], key: chex.PRNGKey) -> OptUpdateResults:
+    def opt_update(params: nn.ParamTree, data: nn.AINetData, opt_state: Optional[optax.OptState],
+                   key: chex.PRNGKey) -> OptUpdateResults:
         (loss, aux_data), grad = loss_and_grad(params, key, data)
         updates, opt_state = optimizer.update(grad, opt_state, params)
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss, aux_data
+
     return opt_update
 
 
 def make_training_step(optimizer_step: OptUpdate) -> Step:
-
     @functools.partial(constants.pmap, donate_argnums=(2))
     def step(data: nn.AINetData,
              params: nn.ParamTree,
              state: Optional[optax.OptState],
-             key: chex.PRNGKey,) -> StepResults:
+             key: chex.PRNGKey, ) -> StepResults:
         mcmc_key, loss_key = jax.random.split(key, num=2)
         new_params, new_state, loss, aux_data = optimizer_step(params, data, state, loss_key)
         return data, new_params, new_state, loss, aux_data
+
     return step
 
 
 def main(atoms: jnp.array,
          charges: jnp.array,
          spins: jnp.array,
+         tstep: float,
          nelectrons: int,
          natoms: int,
          ndim: int,
          batch_size: int,
          iterations: int,
-         structure: jnp.array,
-         Rn_local: jnp.array,
-         Local_coes: jnp.array,
-         Local_exps: jnp.array,
-         Rn_non_local: jnp.array,
-         Non_local_coes: jnp.array,
-         Non_local_exps: jnp.array,
-         ):
+         structure: jnp.array,):
     print("Quantum Monte Carlo Start running")
-    num_devices = jax.local_device_count() #the amount of GPU per host
-    num_hosts = jax.device_count() // num_devices #the amount of host
+    num_devices = jax.local_device_count()  # the amount of GPU per host
+    num_hosts = jax.device_count() // num_devices  # the amount of host
     logging.info('Start QMC with $i devices per host, across %i hosts.', num_devices, num_hosts)
     if batch_size % (num_devices * num_hosts) != 0:
         raise ValueError('Batch size must be divisible by number of devices!')
-    host_batch_size = batch_size // num_hosts # how many configurations we put on one host
-    device_batch_size = host_batch_size // num_devices # how many configurations we put on one GPU
+    host_batch_size = batch_size // num_hosts  # how many configurations we put on one host
+    device_batch_size = host_batch_size // num_devices  # how many configurations we put on one GPU
     seed = jnp.asarray([1e6 * time.time()])
     seed = int(multihost_utils.broadcast_one_to_all(seed)[0])
     key = jax.random.PRNGKey(seed)
@@ -136,11 +132,12 @@ def main(atoms: jnp.array,
     batch_charges = kfac_jax.utils.replicate_all_local_devices(batch_charges)
     pos, spins = init_electrons(subkey, structure=structure, atoms=atoms, charges=charges,
                                 electrons=spins,
-                                batch_size=host_batch_size, init_width=2)
+                                batch_size=host_batch_size, init_width=0.1)
 
     batch_pos = jnp.reshape(pos, data_shape + (-1,))
     batch_pos = kfac_jax.utils.broadcast_all_local_devices(batch_pos)
     data = nn.AINetData(positions=batch_pos, atoms=batch_atoms, charges=batch_charges)
+    #jax.debug.print("batch_charges:{}", batch_charges)
     spins_total = jnp.reshape(spins, (1, nelectrons)) * jnp.reshape(spins, (nelectrons, 1))
     spins_total_uptriangle = jnp.triu(spins_total, k=1)
     sample = jnp.zeros_like(a=spins_total_uptriangle)
@@ -152,6 +149,7 @@ def main(atoms: jnp.array,
     antiparallel_indices = jnp.array(antiparallel_indices)
     n_parallel = len(parallel_indices[0])
     n_antiparallel = len(antiparallel_indices[0])
+    jax.debug.print("n_parallel:{}", n_parallel)
 
     """we already write the envelope function in the nn.py."""
     network = nn.make_ai_net(ndim=ndim,
@@ -168,40 +166,41 @@ def main(atoms: jnp.array,
     params = network.init(subkey)
     params = kfac_jax.utils.replicate_all_local_devices(params)
     signed_network = network.apply
+    #jax.debug.print("charges:{}", charges)
 
     def log_network(*args, **kwargs):
         phase, mag = signed_network(*args, **kwargs)
-        return mag + 1.j*phase
+        return mag + 1.j * phase
 
     print('''--------------Main training-------------''')
     """to be continued...21.12.2024"""
     print('''--------------Start Monte Carlo process------------''')
     sharded_key = kfac_jax.utils.make_different_rng_key_on_all_devices(key)
     sharded_key, subkeys = kfac_jax.utils.p_split(sharded_key)
-    mc_step = mcstep.main_monte_carlo(f=signed_network, key=subkeys, params=params, batch_size=batch_size)
+    mc_step = mcstep.main_monte_carlo(f=signed_network,
+                                      key=subkeys,
+                                      params=params,
+                                      batch_size=batch_size,
+                                      tstep=tstep,
+                                      ndim=ndim,
+                                      nelectrons=nelectrons)
     """to be continued...24.12.2024."""
 
     """we need add the pseudopotential module into the hamiltonian module."""
     """be aware of the list_l, this variable means the angular momentum function max indice in pseudopotential file.7.1.2025."""
-    localenergy = pphamiltonian.local_energy(signed_network=signed_network,
-                                             lognetwork=log_network,
-                                             rn_local=Rn_local,
-                                             local_coes=Local_coes,
-                                             local_exps=Local_exps,
-                                             rn_non_local=Rn_non_local,
-                                             non_local_coes=Non_local_coes,
-                                             non_local_exps=Non_local_exps,
-                                             natoms=natoms,
-                                             nelectrons=nelectrons,
-                                             ndim=ndim,
-                                             list_l=0)
+
+    localenergy = hamiltonian.local_energy(
+        signed_network=signed_network,
+        natoms=natoms,
+        nelectrons=nelectrons,
+        ndim=ndim)
 
     """so far, we have not constructed the pp module. Currently, we only execute all electrons calculation.  """
     evaluate_loss = qmc_loss_functions.make_loss(log_network, local_energy=localenergy)
     """18.10.2024, we will continue later."""
 
     def learning_rate_schedule(t_: jnp.array, rate=0.05, delay=1.0, decay=10000) -> jnp.array:
-        return rate * jnp.power(1.0/(1.0 + (t_/delay)), decay)
+        return rate * jnp.power(1.0 / (1.0 + (t_ / delay)), decay)
 
     """the setup of adam optimzier."""
     optimizer = optax.chain(optax.scale_by_adam(b1=0.9, b2=0.999, eps=1.0e-9, eps_root=0.0),
@@ -219,6 +218,4 @@ def main(atoms: jnp.array,
         data = mc_step(nsteps=50, data=data)
         data, params, opt_state, loss, aux_data, = step(data, params, opt_state, subkeys)
 
-    #return signed_network, data, params, log_network
-
-
+    # return signed_network, data, params, log_network

AIQMCrelease1/wavefunction/nn.py
@@ -182,6 +182,7 @@ def make_orbitals(natoms: int,
                   num_angular: int,
                   n_parallel: int,
                   n_antiparallel: int,
+                  charges: jnp.array,
                   parallel_indices: jnp.array,
                   antiparallel_indices: jnp.array,
                   equivariant_layers: Tuple[InitLayersFn, ApplyLayersFn],) -> ...:
@@ -198,7 +199,7 @@ def make_orbitals(natoms: int,
         dims_orbital_in, params['layers'] = equivariant_layers_init(subkey)
         output_dims = dims_orbital_in
         params['envelope'] = envelope.init(natoms=natoms, nelectrons=nelectrons)
-        params['jastrow_ae'] = jastrow_ae_init(nelectron=nelectrons, charges=jnp.array([4, 6, 6]))
+        params['jastrow_ae'] = jastrow_ae_init(nelectron=nelectrons, charges=charges)
         params['jastrow_ee'] = jastrow_ee_init(n_parallel=n_parallel, n_antiparallel=n_antiparallel)
         orbitals = []
         diffuse_coefficients = []
@@ -239,7 +240,7 @@ def make_orbitals(natoms: int,
                                            parallel_indices=parallel_indices,
                                            antiparallel_indices=antiparallel_indices,
                                            params=params['jastrow_ee'])/nelectrons)
-        wave_function = jastrow*orbitals_end
+        wave_function = jastrow * orbitals_end
         return wave_function
 
     return init, apply
@@ -264,6 +265,7 @@ def make_ai_net(ndim: int,
                                                   num_angular=num_angular,
                                                   n_parallel=n_parallel,
                                                   n_antiparallel=n_antiparallel,
+                                                  charges=charges,
                                                   parallel_indices=parallel_indices,
                                                   antiparallel_indices=antiparallel_indices,
                                                   equivariant_layers=equivariant_layers)

ferminet/train.py
@@ -349,11 +349,11 @@ def make_kfac_training_step(
 
     # Optimization step
     jax.debug.print("-------------------")
-    jax.debug.print("params:{}", params)
-    jax.debug.print("old_data:{}", data)
-    jax.debug.print("loss_keys:{}", loss_keys)
-    jax.debug.print("shared_mom:{}", shared_mom)
-    jax.debug.print("shared_damping:{}", shared_damping)
+    #jax.debug.print("params:{}", params)
+    #jax.debug.print("old_data:{}", data)
+    #jax.debug.print("loss_keys:{}", loss_keys)
+    #jax.debug.print("shared_mom:{}", shared_mom)
+    #jax.debug.print("shared_damping:{}", shared_damping)
     jax.debug.print("--------------------")
     new_params, new_state, stats = optimizer.step(
         params=params,
@@ -363,7 +363,7 @@ def make_kfac_training_step(
         momentum=shared_mom,
         damping=shared_damping,
     )
-    jax.debug.print("new_params:{}", new_params)
+    #jax.debug.print("new_params:{}", new_params)
     if reset_if_nan and jnp.isnan(stats['loss']):
       new_params = old_params
       new_state = old_state
@@ -857,8 +857,8 @@ def train(cfg: ml_collections.ConfigDict, writer_manager=None):
     num_resets = 0  # used if reset_if_nan is true
     for t in range(t_init, cfg.optim.iterations):
       sharded_key, subkeys = kfac_jax.utils.p_split(sharded_key)
-      jax.debug.print("subkeys:{}", subkeys)
-      jax.debug.print("data_input:{}", data)
+      #jax.debug.print("subkeys:{}", subkeys)
+      #jax.debug.print("data_input:{}", data)
       data, params, opt_state, loss, aux_data, pmove = step(
           data,
           params,