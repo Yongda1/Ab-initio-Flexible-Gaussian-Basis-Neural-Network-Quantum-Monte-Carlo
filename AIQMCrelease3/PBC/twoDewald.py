caba16bfd9ac4d6a22523f177f5a45dd7563904c
Yongda Huang <yongdahuang123@gmail.com>
2025-03-04 03:14:20 +0100

    turn to 2D-ewald summation

--- AIQMCrelease3/PBC/twoDewald.py
+++ AIQMCrelease3/PBC/twoDewald.py
@@ -1,2 +1,12 @@
 """create the function for two dimensional ewald summation."""
+import jax
 import jax.numpy as jnp
+from AIQMCrelease3.Energy import pphamiltonian
+
+atoms = jnp.array([[0.0, 0.0, 0.0], [2/3, 1/3, 0.0]])
+charges = jnp.array([4.0, 4.0])
+spins = jnp.array([1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0])
+lattice = jnp.array([[0.5 * jnp.sqrt(3), 0.5, 0],
+                     [0.5 * jnp.sqrt(3), -0.5, 0],
+                     [0, 0, 10]])
+jax.debug.print("lattice:{}", jnp.linalg.norm(lattice, axis=-1))
